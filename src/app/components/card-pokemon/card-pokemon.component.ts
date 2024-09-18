b655cd4b5065e398bb6736bbf6c61cf894c5f3fc
jere <jere77lagos@gmail.com>
2024-09-18 17:56:03 +0200

    implement img styles component

---
+++ src/app/components/card-pokemon/card-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-pokemon',
+  templateUrl: './card-pokemon.component.html',
+  styleUrl: './card-pokemon.component.scss'
+})
+export class CardPokemonComponent {
+
+}
---
+++ src/app/components/img-pokemon/img-pokemon.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'img-pokemon',
+  templateUrl: './img-pokemon.component.html',
+  styleUrl: './img-pokemon.component.scss'
+})
+export class ImgPokemonComponent {
+
+}
